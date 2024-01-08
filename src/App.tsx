b58eac28bcd2f107c5caede6ddbf0c41adeb1cdb
Radosław Siek <Borderlandsmaniak@gmail.com>
2024-01-08 21:46:58 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="tarcking-wide text-3xl uppercase text-slate-500">
+        Hello World
+      </h1>
+    </div>
+  );
+}
+
+export default App;
